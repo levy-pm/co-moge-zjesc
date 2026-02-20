acbc6bceff984501a0c3bcf3e069dbd4fc715b38
Krzysztof Lewandowski <klewandowski638@gmail.com>
2026-02-20 18:03:38 +0100

    Update deployment files and local cache artifacts

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,9 @@
 import os
 import sys
 
-# Pobieramy pełną ścieżkę do folderu, w którym jest ten plik
 path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, path)
 
-# Ustawiamy moduł ustawień - upewnij się, że 'strona_kuchnia' to na pewno 
-# nazwa folderu zawierającego plik settings.py
 os.environ['DJANGO_SETTINGS_MODULE'] = 'strona_kuchnia.settings'
 
 from django.core.wsgi import get_wsgi_application
